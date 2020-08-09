c678f2c9fc9e159a0d5266c0d2e3dcaeb1bf7e44
cockochan <56898295+cockochan@users.noreply.github.com>
2020-08-09 23:26:18 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+/* eslint-disable linebreak-style */
 import React, { useEffect, useState } from "react";
-
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+} from "react-router-dom";
 import "./App.css";
 import { getMessage } from "./service";
-import logo from "./logo.svg";
+import Mentors from "./Components/Mentors";
 
 export function App() {
 	const [message, setMessage] = useState("Loading...");
@@ -17,6 +23,14 @@ export function App() {
 				<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
 				<h1 className="message" data-qa="message">{message}</h1>
 			</div>
+			<Router>
+
+				<Switch>
+					<Route path="/Mentors">
+						<Mentors />
+					</Route>
+				</Switch>
+			</Router>
 		</main>
 	);
 }
---
+++ client/src/Components/Mentors.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+import React from "react";
+
+const Mentors = () => (
+	<div>Hi, dear mentor</div>
+);
+
+export default Mentors;
