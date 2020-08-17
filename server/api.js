301db82868bb36cdc52732d9d84c3b56a05d1e88
Nader Akhgari <nader90akhgari@gmail.com>
2020-08-17 22:56:06 +0100

    add 4 endpoint to get and post questions and quizes

--- server/api.js
+++ server/api.js
@@ -2,8 +2,11 @@
 import { Router } from "express";
 
 import { getClient } from "./db";
+const cors = require("cors");
+
 
 const router = new Router();
+router.use(cors());
 
 router.get("/", (_, res, next) => {
 	const client = getClient();
@@ -14,6 +17,70 @@ router.get("/", (_, res, next) => {
 		}
 		res.json({ message: "Hello, world!" });
 		client.close();
+	});
+});
+
+router.get("/questions",(req, res)=>{
+	const client = getClient();
+	client.connect(()=>{
+		const db = client.db("quiz");
+		const collection = db.collection("questions");
+		collection.find().toArray((err, questions)=>{
+			if(err){
+				res.status(500).send(err);
+			}else{
+				res.status(200).send(questions);
+			}
+		});
+	});
+});
+
+router.post("/question",(req, res)=>{
+	const client = getClient();
+	client.connect(()=>{
+		const db = client.db("quiz");
+		const collection = db.collection("questions");
+		const question = req.body;
+
+		collection.insertOne(question,(err, question)=>{
+			if(err){
+				res.status(500).send(err);
+			}else{
+				res.status(200).send(question);
+			}
+		});
+	});
+});
+
+router.post("/quiz",(req, res)=>{
+	const client = getClient();
+	client.connect(()=>{
+		const db = client.db("quiz");
+		const collection = db.collection("quizes");
+		const question = req.body;
+
+		collection.insertOne(question,(err, quiz)=>{
+			if(err){
+				res.status(500).send(err);
+			}else{
+				res.status(200).send(quiz);
+			}
+		});
+	});
+});
+
+router.get("/quizes",(req, res)=>{
+	const client = getClient();
+	client.connect(()=>{
+		const db = client.db("quiz");
+		const collection = db.collection("quizes");
+		collection.find().toArray((err, questions)=>{
+			if(err){
+				res.status(500).send(err);
+			}else{
+				res.status(200).send(questions);
+			}
+		});
 	});
 });
 
