788eb872aec911bd3a026c149eeb9c832725ce31
cockochan <56898295+cockochan@users.noreply.github.com>
2020-08-18 02:05:45 +0300

    Revert "Merge remote-tracking branch 'origin/backend-endpoint-get-and-post'"
    This reverts commit df372677a3599bfeb1f57103485f0ee9b7bbba4c, reversing
    changes made to 1a80188f34957a0bf9ab35f4eaab84e914e04415.

--- server/db.js
+++ server/db.js
@@ -1,5 +1,4 @@
 import { MongoClient } from "mongodb";
-require("dotenv").config();
 
 const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/cyf";
 const configuration = { useNewUrlParser: true, useUnifiedTopology: true };
