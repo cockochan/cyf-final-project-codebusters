dfa16650b92a08cc91b898d1a0a7825cd9c04ecf
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2020-08-27 12:25:46 +0100

    Revert "get and post results and get results by quiz name ..."
    This reverts commit b5b32845dedbab2de947d0f78367843acb867dbe.

--- server/api.js
+++ server/api.js
@@ -1,6 +1,5 @@
 
 import { Router } from "express";
-import mongodb from "mongodb";
 
 import { getClient } from "./db";
 const cors = require("cors");
@@ -76,90 +75,6 @@ router.get("/quizes",(req, res)=>{
 		const db = client.db("quiz");
 		const collection = db.collection("quizes");
 		collection.find().toArray((err, questions)=>{
-			if(err){
-				res.status(500).send(err);
-			}else{
-				res.status(200).send(questions);
-			}
-		});
-	});
-});
-
-router.get("/quizes/search",(req, res)=>{
-	const client = getClient();
-	client.connect(()=>{
-		const db = client.db("quiz");
-		const collection = db.collection("quizes");
-		const quiz = { quiz_name:req.query.quiz_name };
-		collection.findOne(quiz,(err, questions)=>{
-			if(err){
-				res.status(500).send(err);
-			}else{
-				res.status(200).send(questions);
-			}
-		});
-	});
-});
-
-router.get("/results/search",(req, res)=>{
-	const client = getClient();
-	client.connect(()=>{
-		const db = client.db("quiz");
-		const collection = db.collection("results");
-		const quiz = { name:req.query.name };
-		collection.find(quiz).toArray((err, results)=>{
-			if(err){
-				res.status(500).send(err);
-			}else{
-				res.status(200).send(results);
-			}
-		});
-	});
-});
-router.get("/results",(req, res)=>{
-	const client = getClient();
-	client.connect(()=>{
-		const db = client.db("quiz");
-		const collection = db.collection("results");
-		collection.find().toArray((err, results)=>{
-			if(err){
-				res.status(500).send(err);
-			}else{
-				res.status(200).send(results);
-			}
-		});
-	});
-});
-
-router.post("/results",(req, res)=>{
-	const client = getClient();
-	client.connect(()=>{
-		const db = client.db("quiz");
-		const collection = db.collection("results");
-		const question = req.body;
-
-		collection.insertOne(question,(err, result)=>{
-			if(err){
-				res.status(500).send(err);
-			}else{
-				res.status(200).send(result.ops[0]);
-			}
-		});
-	});
-});
-
-router.get("/question/:id",(req, res)=>{
-	const client = getClient();
-	client.connect(()=>{
-		const db = client.db("quiz");
-		const collection = db.collection("questions");
-
-		const questionId = req.params.id;
-		if (!mongodb.ObjectID.isValid(questionId)) {
-			return res.status(400).json("the ID is not valid");
-		}
-		const id = mongodb.ObjectID(questionId);
-		collection.findOne(id,(err, questions)=>{
 			if(err){
 				res.status(500).send(err);
 			}else{
