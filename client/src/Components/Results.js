8105e48387b004c288444c364c95b57ae7d1f8fb
cockochan <56898295+cockochan@users.noreply.github.com>
2020-08-30 00:36:55 +0300

    stash need it
    stash need it

--- client/src/Components/Results.js
+++ client/src/Components/Results.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Results(props) {
+	const [studentNames, setStudentNames]=useState([]);
+	const [answeredQuestons, setAnsweredQuestons]=useState([]);
+	const [allResults, setAllResults]=useState(null);
+	const fetchResults =()=>{
+		fetch("http://localhost:3100/api/results/")
+			.then((response)=>response.json())
+			.then((data)=>setAllResults(data));
+	};
+	useEffect(() => {
 
-	if(props.quizes){
+		fetchResults();
+	},[]);
+
+	const quizzToSeeResultsChosen=(e)=>{
+		console.log(e.target.value);
+	};
+	if(props.quizes&&allResults){
 		return (
 			<div className='col-12'>
-				we need to do something to have results
-				{/* <h1>Welcome to QuizzTime</h1>
 
-				<select name="quizzez" id="quizzez" onChange={(e) => quizzChosen(e.target.value)}>
+				<h1>Welcome to QuizzTime</h1>
+
+				<select name="quizzez" id="quizzez" onChange={(e) => quizzToSeeResultsChosen(e)}>
 					<option >select a quiz</option>
 
 					{props.quizes.map((quiz)=>{
 						return(<option value={quiz.name}>{quiz.name}</option>);
 					})}
 
-		  </select> */}
+		  </select>
+		  {allResults.map((res)=>{
+					if(!answeredQuestons.find((answeredQuestion)=>res.question_id=answeredQuestion)) {
+						setAnsweredQuestons([...answeredQuestons,res.question_id]);
+					}
+				})}
 		  </div>
 		);
 	} else{
