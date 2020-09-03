332e10190d840aee72f9838772ca19eedbbe0471
cockochan <56898295+cockochan@users.noreply.github.com>
2020-09-03 13:35:50 +0300

    Ready to pull

--- client/src/Components/Results.js
+++ client/src/Components/Results.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 
 export default function Results(props) {
 	const [studentNames, setStudentNames]=useState([]);
-	const [selQuizQuestions, setSelQuizQuestions]=useState([]);
+	const [selectedQuizQuestions, setSelectedQuizQuestions]=useState([]);
 	const [quizRoute,setQuizRoute]=useState("");
 	const [allResults, setAllResults]=useState(null);
 	const [quizSelected, setQuizSelected]=useState(null);
+	const [attemptNumber, setAttemptNumber]=useState(0);
 	const fetchResults =()=>{
 		fetch(`http://localhost:3100/api/results/${quizRoute}`)
 			.then((response)=>response.json())
@@ -14,19 +15,25 @@ export default function Results(props) {
 			}
 			)	.catch((err) => console.error(err));
 	};
-
+	const increaseAttemptNumber = () =>setAttemptNumber(attemptNumber+1);
+	const decreaseAttemptNumber =()=> {
+		if(attemptNumber>0){
+			setAttemptNumber(attemptNumber-1);
+		} else{
+			return("can't increase");
+		}
+	};
 	const findQuestionResult=(actualQuestionId, oneName)=>{
-		let resultsforThisQuestion = allResults.filter((el)=>el.question_id==actualQuestionId);
+		let resultsforThisQuestion = allResults.filter((result)=>result.question_id==actualQuestionId);
 
-		let studentsAttemptsOnQuestion = resultsforThisQuestion.filter((el)=>el.studentName==oneName);
+		let studentsAttemptsOnQuestion = resultsforThisQuestion.filter((result)=>result.studentName==oneName);
 		if(studentsAttemptsOnQuestion.length==0){
 			return("unknown");
 		}
-		if(studentsAttemptsOnQuestion.length>0){
-			console.log({ studentsAttemptsOnQuestion });
+		if(studentsAttemptsOnQuestion[attemptNumber]===undefined){
+			return("unknown");
 		}
-
-		if(studentsAttemptsOnQuestion!==undefined&&studentsAttemptsOnQuestion[0].correct){
+		if(studentsAttemptsOnQuestion!==undefined&&studentsAttemptsOnQuestion[attemptNumber].correct){
 			return("corect");
 		} else{
 			return("incorect");
@@ -40,8 +47,8 @@ export default function Results(props) {
 		}
 		if(studentAnsweredQuestion.length>0){
 		}
-		if(studentAnsweredQuestion!==undefined&&studentAnsweredQuestion[0]){
-			return(studentAnsweredQuestion[0].value);
+		if(studentAnsweredQuestion!==undefined&&studentAnsweredQuestion[attemptNumber]){
+			return(studentAnsweredQuestion[attemptNumber].value);
 		}
 	};
 	useEffect(() => {
@@ -53,9 +60,8 @@ export default function Results(props) {
 				let selectedQuizQuestions= [];
 				selectedQuizQuestions = quizSelected.questions_id.map((selId)=>{
 					let found = (props.questions.find((question)=>question._id==selId));
-					console.log({ found });
 					selectedQuizQuestions.push(found);
-					setSelQuizQuestions(selectedQuizQuestions);
+					setSelectedQuizQuestions(selectedQuizQuestions);
 				}
 				);
 			};
@@ -82,7 +88,7 @@ export default function Results(props) {
 	const quizzToSeeResultsChosen=(e)=>{
 		setQuizRoute(e.target.value);
 		const selectedQuiz=props.quizes.find((quiz)=>(quiz._id==e.target.value));
-		setSelQuizQuestions([]);
+		setSelectedQuizQuestions([]);
 		setQuizSelected(selectedQuiz);
 	};
 	if(allResults=="not found!"){
@@ -101,30 +107,33 @@ export default function Results(props) {
 	} else if(props.quizes){
 		return (
 			<div  className='col-12 centered'>
+				<h1>Chosse a quizz to see student results</h1>
 				<select name="quizzez" className="quizzez" onChange={quizzToSeeResultsChosen}>
-					<option >select a quiz</option>
+					<option value="" disabled selected hidden>select a quiz</option>
 					{props.quizes.map((quiz)=>{
 						return(<option value={quiz._id} >{quiz.name}</option>);
 					})}
 		  </select>
-				<div>
-					{allResults&&allResults!=="not found!"?<table>
-						<thead>
-							<tr><td></td>{selQuizQuestions?selQuizQuestions.map((question)=>{
-								return(<th className='col-2'>{question!==undefined?<p>{question.question}</p>:<p>Loading question</p>}</th>);
-							}):<tr></tr>}</tr>
-							{studentNames&&quizSelected?studentNames.filter(Boolean).map((oneName)=>{
-								return(
-									<tr><td>{oneName}</td>{selQuizQuestions?selQuizQuestions.map((question)=>{
-										return(<th className={findQuestionResult(question._id, oneName)}>{question!==undefined?<p>{findStudenAnswerResult(question._id, oneName)}</p>:<p>Loading question</p>}</th>);
-									}):null}</tr>
-								);
-							}
-							):null}
-						</thead>
+		  <div className='centered'><h2>attempt number {attemptNumber+1} </h2><p>(change to see if some students made multiple attempts)</p></div>
+				<button onClick={decreaseAttemptNumber}>decrease attempt number </button>
+				<button onClick={increaseAttemptNumber}>increase attempt number </button>
+				{quizSelected&&allResults&&allResults!=="not found!"?<table>
+					<thead>
+						<tr><td></td>{selectedQuizQuestions?selectedQuizQuestions.map((question)=>{
+							return(<th className='col-2'>{question!==undefined?<p>{question.question}</p>:<p>Loading question</p>}</th>);
+						}):<tr></tr>}</tr>
+						{studentNames&&quizSelected?studentNames.filter(Boolean).map((oneName)=>{
+							return(
+								<tr><td>{oneName}</td>{selectedQuizQuestions?selectedQuizQuestions.map((question)=>{
+									return(<th className={findQuestionResult(question._id, oneName)}>{question!==undefined?<p>{findStudenAnswerResult(question._id, oneName)}</p>:<p>Loading question</p>}</th>);
+								}):null}</tr>
+							);
+						}
+						):null}
+					</thead>
 
-					</table>:null}
-		  </div></div>
+				</table>:null}
+		 </div>
 		  );
 	} else{
 		return(<div>no props</div>);
