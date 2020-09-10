eedb12b29fb445b2c24788d1f96e5fb53673265c
cockochan <56898295+cockochan@users.noreply.github.com>
2020-09-10 21:15:57 +0300

    home welcome

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -3,7 +3,10 @@ import Quizpic from "../quiztime.png";
 export default function Home() {
 	return (
 		<div className='container'>
-			<div className=" welcome col-3">Welcome to QuizzTime</div>
+			<div className='row welcomeContainer'>
+				<div className=" welcomeBig col-6">Q</div>
+				<div className=" welcome col-4">Welcome to QuizzTime</div>
+			</div>
 			{/* <img className='quizzTimeLogo' src={Quizpic} /> */}
 		</div>
 	);
