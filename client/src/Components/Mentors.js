e0961834507ae78cc04ce76b23547273e5f32ec8
cockochan <56898295+cockochan@users.noreply.github.com>
2020-09-12 22:34:52 +0300

    Mentors added

--- client/src/Components/Mentors.js
+++ client/src/Components/Mentors.js
@@ -134,56 +134,69 @@ export default function Mentors(props) {
 	};
 	if (filteredQuestionsByTag) {
 		return (
-
-			<div className="row">
-      <nav className="navbar">
-					<Link to="/Results" exact="true" className="link-button">
+			<div className='container'>
+				<div className="row">
+					<nav className="navbar">
+						<Link to="/" exact="true">
+							<img
+								src="https://codeyourfuture.io/wp-content/uploads/2019/03/cyf_brand.png"
+								alt="cyf_brand.png"
+								className="cyf-log"
+							/>
+						</Link>
+						<Link to="/Results" exact="true" className="link-button">
             			Quiz Results
-					</Link>
-					<Link to="/NewQuestion" exact="true" className="link-button">
+						</Link>
+						<Link to="/NewQuestion" exact="true" className="link-button">
             			New Question
-					</Link>
-				</nav>
-				<RunQuiz
-					quizzes={props.quizzes}
-					setRoute={props.setRoute}
-					setCode={props.setCode}
-					code={props.code}
-					setQuizId={props.setQuizId}
-					setData={props.setData}
-				/>
-				<div className="filterButtons col-6">
-					<button onClick={autofillQuizz}>autofill quiz</button>
-					<button onClick={resetFilters}>reset filters</button>
-					{tagsCollection.map((tag, index) => {
-						return (
-							<button value={tag} onClick={tagClickHandler} key={index}>
-								{tag}
-							</button>
-						);
-					})}
-				</div>
-				<div>
-					 <p>Number of question</p>
+						</Link>
+					</nav>
+					<div className='col-6 topFilterSelect'>
+						<RunQuiz
+							quizzes={props.quizzes}
+							setRoute={props.setRoute}
+							setCode={props.setCode}
+							code={props.code}
+							setQuizId={props.setQuizId}
+							setData={props.setData}
+						/>
+						<div className="filterButtons row">
+							<select  onChange={tagClickHandler}>
+								<option value="" disabled selected hidden>select tag filter</option>
+								{tagsCollection.map((tag, index) => {
+									return (
+										<option value={tag} name={tag} key={index} >{tag}</option>
+									);
+								})}
+							</select>
+							<div>
 					 <select onChange={selectHandler}>
-						<option value="5">5</option>
-						<option value="10">10</option>
-						<option value="15">15</option>
-					</select>
+						 <option selected disabled>Number of question</option>
+									<option value="5">5</option>
+									<option value="10">10</option>
+									<option value="15">15</option>
+								</select>
 					 </div>
-				<div className="col-8 card-block">
-					{filteredQuestionsByTag.map((question, index) => (
-						<div className="col-6" key={index}>
-							<div className=" col-12 card">
+							<button onClick={autofillQuizz}>autofill quiz</button>
+							<button onClick={resetFilters}>reset filters</button>
+						</div>
+					</div>
+					<div className="col-8 card-block">
+						{filteredQuestionsByTag.map((question, index) => (
+
+							<div className=" col-4 xl-col-3 md-col-4 sm-col-12 card" key={index}>
 								<div className="question-and-code-containter">
 									{question.question_code ? (
-										<ReactMarkdown className="code">
-											{question.question_code}
-										</ReactMarkdown>
+										<div>
+											<ReactMarkdown className="code">
+												{question.question_code}
+											</ReactMarkdown></div>
 									) : null}
-									<ReactMarkdown className="question">
-										{question.question}
-									</ReactMarkdown>
+									<div>
+										<ReactMarkdown className="question">
+											{question.question}
+										</ReactMarkdown>
+									</div>
 								</div>
 								<div className="answers">
 									{Object.values(question.answers).map((value, index) => {
@@ -204,55 +217,55 @@ export default function Mentors(props) {
                 Add to Quiz
 								</button>
 							</div>
-						</div>
-					))}
-				</div>
-				<div className="col-4 quiz-questions">
-					<div className="form-title">New quiz</div>
-					<div className="quiz-handler">
-						<button onClick={clearQuiz} className="quiz-button"> clear quiz</button>
-						<input
-							type="text"
-							onKeyUp={newQuizName}
-							placeholder={"Enter quiz name"}
-							className="input"
-						/>
-						<button onClick={submitQuiz} className="quiz-button">Submit</button>
+
+						))}
 					</div>
-					{newQuizQuestions.map((question) => (
-						<div className="col-12 card" key={question.question}>
-							<div className="question-and-code-containter">
-								{question.question_code ? (
-									<ReactMarkdown className="code">
-										{question.question_code}
-									</ReactMarkdown>
-								) : null}
-								<ReactMarkdown className="question">{question.question}</ReactMarkdown>
-							</div>
-							<div className="answers">
-								{Object.entries(question.answers).map(([index, value]) => {
-									return (
-										<div key={index}>
-											<div className="col-6 answer">{value}</div>
-										</div>
-									);
-								})}
-							</div>
-							<button
-								key={question._id + question.question}
-								type="checkbox"
-								checked="checked"
-								id="horns"
-								value={question._id}
-								onClick={removeQuestion}
-								className="quiz-button card-button"
-							>
-                Delete
-							</button>
+					<div className="col-3 quizQuestions">
+						<div className="form-title">New quiz</div>
+						<div className="quiz-handler">
+							<button onClick={clearQuiz} className="quiz-button"> clear quiz</button>
+							<input
+								type="text"
+								onKeyUp={newQuizName}
+								placeholder={"Enter quiz name"}
+								className="input"
+							/>
+							<button onClick={submitQuiz} className="quiz-button">Submit</button>
 						</div>
-					))}
-				</div>
-			</div>
+						{newQuizQuestions.map((question) => (
+							<div className="col-12 card" key={question.question}>
+								<div className="question-and-code-containter">
+									{question.question_code ? (
+										<ReactMarkdown className="code">
+											{question.question_code}
+										</ReactMarkdown>
+									) : null}
+									<ReactMarkdown className="question">{question.question}</ReactMarkdown>
+								</div>
+								<div className="answers">
+									{Object.entries(question.answers).map(([index, value]) => {
+										return (
+											<div key={index}>
+												<div className="col-6 answer">{value}</div>
+											</div>
+										);
+									})}
+								</div>
+								<button
+									key={question._id + question.question}
+									type="checkbox"
+									checked="checked"
+									id="horns"
+									value={question._id}
+									onClick={removeQuestion}
+									className="quiz-button card-button"
+								>
+                Delete
+								</button>
+							</div>
+						))}
+					</div>
+				</div></div>
 		);
 	} else {
 		return <div>No questions loaded</div>;
